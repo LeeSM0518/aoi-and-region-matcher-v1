255bcf99c1cc1009ef1ac8876cbfd36f194501a4
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-05 10:37:59 +0900

    #1 프로젝트 초기 구성
    * Spring Initializr 활용한 프로젝트 설정
      * Java/Spring Web/Lombock 기반의 프로젝트 구성
    * gitignore 내용 변경
    
    Resolves: #1
    See also: None

---
+++ src/main/java/io/wisoft/aoiandregionmetcherv1/AoiAndRegionMetcherV1Application.java
@@ -0,0 +1,13 @@
+package io.wisoft.aoiandregionmetcherv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoiAndRegionMetcherV1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AoiAndRegionMetcherV1Application.class, args);
+  }
+
+}
---
+++ src/test/java/io/wisoft/aoiandregionmetcherv1/AoiAndRegionMetcherV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.aoiandregionmetcherv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoiAndRegionMetcherV1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
