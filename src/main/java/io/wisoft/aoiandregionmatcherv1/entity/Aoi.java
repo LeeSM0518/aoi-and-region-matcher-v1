6415350dccf5e0ba2e3f18e7fee153cac644d9b0
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-06 00:06:15 +0900

    #29 관심지역 Entity와 Repository 구현 및 테스트
    - s/main/j/i/w/a/entity/Aoi.java
      - 관심지역 Entity 구현
    - s/main/j/i/w/a/repository/AoiRepository.java
      - 관심지역 Repository 구현
    - s/test/j/i/w/a/repository/AoiRepositoryTest.java
      - 관심지역 Repository 테스트
    
    Resolves: #29
    See also: None

---
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/entity/Aoi.java
@@ -0,0 +1,38 @@
+package io.wisoft.aoiandregionmatcherv1.entity;
+
+import io.wisoft.aoiandregionmatcherv1.dto.Point;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Polygon;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+import static io.wisoft.aoiandregionmatcherv1.entity.factory.PolygonFactory.createPolygon;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public class Aoi {
+
+  @Id
+  @GeneratedValue(generator = "increment")
+  @Column(name = "aoi_id", nullable = false)
+  private Integer id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(columnDefinition = "geometry(Point, 4326)", nullable = false)
+  private Polygon area;
+
+  public Aoi(final String name, final List<Point> area) {
+    this.name = name;
+    this.area = createPolygon(area);
+  }
+
+}
