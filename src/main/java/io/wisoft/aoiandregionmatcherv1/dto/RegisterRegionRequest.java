0f997bfd1a2191404d1c3a6a712c4d5fa717a116
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-05 23:24:15 +0900

    #5 행정 지역의 이름과 전체 지리 정보 저장 API 구현
    * s/main/j/i/w/a/controller/RegionController.java
      * RegionController의 등록 기능 구현
    * s/main/j/i/w/a/dto/RegisterRegionResponse.java
      * Region 등록의 응답 DTO 구현
    * s/main/j/i/w/a/dto/RegisterRegionRequest.java
      * Region 등록의 요청 DTO 수정
        * 이름이 존재하지 않거나 비어있으면 400 응답
    * s/main/resources/application.yml
      * PostgisDialect 추가
    
    * src/test/java/io/wisoft/aoiandregionmatcherv1/controller/RegionControllerTest.java
      * WebClient를 활용한 Region 등록 기능 통합 테스트
    * src/test/api/api.http
      * HTTP 파일을 활용한 Region 등록 기능 통합 테스트
    * src/test/resources/application.yml
      * PostgisDialect 추가
    
    * build.gradle
      * 통합 테스트를 위한 WebFlux 의존 추가
      * HTTP Request의 Body를 검증하기 위한 Validation 의존 추가
    
    Resolves: #5
    See also: None

--- src/main/java/io/wisoft/aoiandregionmatcherv1/dto/RegisterRegionRequest.java
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/dto/RegisterRegionRequest.java
@@ -3,13 +3,21 @@ package io.wisoft.aoiandregionmatcherv1.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class RegisterRegionRequest {
 
+  @NotNull
+  @NotBlank
   private final String name;
+
+  @NotNull
+  @NotEmpty
   private final List<Point> area;
 
 }
---
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/dto/RegisterRegionResponse.java
@@ -0,0 +1,14 @@
+package io.wisoft.aoiandregionmatcherv1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRegionResponse {
+
+  private Integer id;
+
+}
