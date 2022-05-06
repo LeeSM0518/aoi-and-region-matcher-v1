2fe90c241ec97dd8e921a0fab47c42d7646da0e4
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-06 16:53:02 +0900

    #45 가장 가까운 관심 지역 조회 Repository 구현
    - s/main/j/i/w/a/repository/AoiRepository.java
      - 가장 가까운 관심 지역 조회 기능 구현
    - s/test/j/i/w/a/repository/AoiRepositoryTest.java
      - 가장 가까운 관심 지역 조회 테스트 구현
    
    Resolves: #45
    See also: None

--- src/main/java/io/wisoft/aoiandregionmatcherv1/repository/AoiRepository.java
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/repository/AoiRepository.java
@@ -1,9 +1,21 @@
 package io.wisoft.aoiandregionmatcherv1.repository;
 
 import io.wisoft.aoiandregionmatcherv1.entity.Aoi;
+import io.wisoft.aoiandregionmatcherv1.entity.Aoi.AoiRow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AoiRepository extends JpaRepository<Aoi, Integer> {
+
+  @Query(value = "SELECT a.aoi_id AS id, " +
+      "a.name, " +
+      "CAST(st_astext(a.area) AS VARCHAR) as areaText " +
+      "FROM aoi a, st_setsrid(st_point(:x, :y), 4326) p " +
+      "ORDER BY st_distance(p, a.area) " +
+      "LIMIT 1", nativeQuery = true)
+  AoiRow findAoiClosestToPoint(@Param(value = "x") Double x, @Param(value = "y") Double y);
+
 }
