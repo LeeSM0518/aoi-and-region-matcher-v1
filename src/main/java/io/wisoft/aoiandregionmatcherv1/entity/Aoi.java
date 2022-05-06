9225cafffe8abec9534966a712ec660d405ae2ee
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-06 12:58:29 +0900

    #36 행정지역에 포함되는 관심지역 조회 Repository
    - s/main/j/i/w/a/entity/Aoi.java
      - Repository로부터 조회된 Aoi 행 Mapper 인터페이스 추가
    - s/main/j/i/w/a/entity/factory/PolygonFactory.java
      - String으로부터 Polygon으로 변환하는 Factory 메서드 추가
    - s/main/j/i/w/a/repository/RegionRepository.java
      - 행정지역에 포함되는 관심지역 조회 기능 구현
    - s/main/j/i/w/a/exception/StringToPolygonParseException.java
      - String을 Polygon으로 파싱할 때 발생하는 예외
    
    - s/test/j/i/w/a/entity/factory/PolygonFactoryTest.java
      - 새로 구현된 Factory 메서드 테스트 구현
    - s/test/j/i/w/a/repository/RegionRepositoryTest.java
      - 행정지역에 포함되는 관심지역 조회 기능 테스트 구현
    
    Resolves: #36
    See also: None

--- src/main/java/io/wisoft/aoiandregionmatcherv1/entity/Aoi.java
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/entity/Aoi.java
@@ -1,6 +1,7 @@
 package io.wisoft.aoiandregionmatcherv1.entity;
 
 import io.wisoft.aoiandregionmatcherv1.dto.Point;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.locationtech.jts.geom.Polygon;
@@ -12,10 +13,12 @@ import javax.persistence.Id;
 import java.util.List;
 
 import static io.wisoft.aoiandregionmatcherv1.entity.factory.PolygonFactory.createPolygon;
+import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
 @Getter
 public class Aoi {
 
@@ -33,6 +36,18 @@ public class Aoi {
   public Aoi(final String name, final List<Point> area) {
     this.name = name;
     this.area = createPolygon(area);
+  }
+
+  public interface AoiRow {
+
+    Integer getId();
+    String getName();
+    String getAreaText();
+    default Aoi toEntity() {
+      final Polygon area = createPolygon(getAreaText());
+      return new Aoi(getId(), getName(), area);
+    }
+
   }
 
 }
--- src/main/java/io/wisoft/aoiandregionmatcherv1/entity/factory/PolygonFactory.java
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/entity/factory/PolygonFactory.java
@@ -1,19 +1,22 @@
 package io.wisoft.aoiandregionmatcherv1.entity.factory;
 
 import io.wisoft.aoiandregionmatcherv1.dto.Point;
+import io.wisoft.aoiandregionmatcherv1.exception.RingNotClosedException;
+import io.wisoft.aoiandregionmatcherv1.exception.StringToPolygonParseException;
 import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.CoordinateSequence;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LinearRing;
 import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class PolygonFactory {
   private static final GeometryFactory geometryFactory = new GeometryFactory();
+  private static final WKTReader reader = new WKTReader();
 
   public static Polygon createPolygon(List<Point> points) {
     try {
@@ -23,6 +26,14 @@ public class PolygonFactory {
       return new Polygon(linear, null, geometryFactory);
     } catch (IllegalArgumentException exception) {
       throw new RingNotClosedException();
+    }
+  }
+
+  public static Polygon createPolygon(String polygon) {
+    try {
+      return (Polygon) reader.read(polygon);
+    } catch (ParseException exception) {
+      throw new StringToPolygonParseException();
     }
   }
 
