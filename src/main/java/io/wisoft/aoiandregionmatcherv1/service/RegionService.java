ca2d09ab33c104efc7ef23c5bfe006cb93b7f8f0
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-05-06 13:32:11 +0900

    #41 Service의 행정지역에 포함된 관심지역 조회 구현
    - s/main/j/i/w/a/dto/FindAoisIncludedRegionRequest.java
      - 행정지역에 포함된 관심지역 조회 Request DTO
    - s/main/j/i/w/a/service/RegionService.java
      - 행정지역에 포함된 관심지역 조회 기능 구현
    - s/test/j/i/w/a/service/RegionServiceTest.java
      - 행정지역에 포함된 관심지역 조회 테스트 구현
    
    Resolves: #41
    See also: None

--- src/main/java/io/wisoft/aoiandregionmatcherv1/service/RegionService.java
+++ src/main/java/io/wisoft/aoiandregionmatcherv1/service/RegionService.java
@@ -1,23 +1,38 @@
 package io.wisoft.aoiandregionmatcherv1.service;
 
+import io.wisoft.aoiandregionmatcherv1.dto.FindAoisIncludedRegionRequest;
 import io.wisoft.aoiandregionmatcherv1.dto.RegisterRegionRequest;
+import io.wisoft.aoiandregionmatcherv1.entity.Aoi;
+import io.wisoft.aoiandregionmatcherv1.entity.Aoi.AoiRow;
 import io.wisoft.aoiandregionmatcherv1.entity.Region;
 import io.wisoft.aoiandregionmatcherv1.repository.RegionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
 public class RegionService {
 
   private final RegionRepository regionRepository;
+
   @Transactional
   public Integer register(RegisterRegionRequest request) {
     final Region region = new Region(request.getName(), request.getArea());
     final Region savedRegion = regionRepository.save(region);
     return savedRegion.getId();
+  }
+
+  public List<Aoi> findAoisIncludedRegion(FindAoisIncludedRegionRequest request) {
+    return regionRepository
+        .findAoisIncludedRegion(request.getRegionId())
+        .stream()
+        .map(AoiRow::toEntity)
+        .collect(toList());
   }
 
 }
